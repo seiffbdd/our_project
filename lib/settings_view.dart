bee5f334310641d7cd52b4c33bd37eecf14a4804
Seif Elnawawy <elnawawyseif@gmail.com>
2025-04-20 16:13:33 +0200

    add some views

---
+++ lib/settings_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings View')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Settings will be here.'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back to Home'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
